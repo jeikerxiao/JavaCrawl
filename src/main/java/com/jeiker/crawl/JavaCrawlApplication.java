84fcd1a2eab9dd6c8f1bf63e99baa94159b794b4
jeikerxiao <jeiker@126.com>
2018-02-27 10:45:47 +0800

    init project
    init project

---
+++ src/main/java/com/jeiker/crawl/JavaCrawlApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.crawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrawlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrawlApplication.class, args);
+	}
+}
